3e768b599d4e7c3a58a53bafaca7d09aea00c59f
Perspective App <164425180+iperspective@users.noreply.github.com>
2024-06-05 19:28:29 -0300

    initial commit

---
+++ ethereum.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Aquí puedes agregar cualquier código JavaScript que necesites para la página de Ethereum
+    console.log('Ethereum - Datos de mercado');
+});
